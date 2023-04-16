c63987d2b0f31a39303c7254a65d07c969faf2f9
Anush008 <anushshetty90@gmail.com>
2023-04-16 21:07:45 +0530

    update: Pdf content to chunks

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 use actix_web::{web, App, HttpServer, Responder, post};
 use lopdf::Document;
+use regex::Regex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref RE_NL: Regex = Regex::new(r"\n").unwrap();
+    static ref RE_SPACE: Regex = Regex::new(r"\s+").unwrap();
+}
+
+fn preprocess(text: String) -> String {
+    RE_SPACE
+        .replace_all(&RE_NL.replace_all(&text, " "), " ")
+        .to_string()
+}
 
 #[post("/pdf")]
 async fn extract_pdf(pdf: web::Bytes) -> impl Responder {
+    //TODO: Move to utils
     let doc = Document::load_mem(&pdf.to_vec()).unwrap();
     let pages  = doc.get_pages();
-    let length = pages.len();
-
-    length.to_string()
+    let mut chunks: Vec<String> = Vec::new();
+    for page_num in 1..=pages.len() {
+        let text = doc.extract_text(&[page_num.try_into().unwrap()]).unwrap();
+        let text = preprocess(text);
+        let mut chunk:Vec<String> = text.chars().collect::<Vec<char>>().chunks(200)
+        .map(|chunk| chunk.iter().collect::<String>()).map(|s| format!("[{page_num}]{s}")).collect();
+        chunks.append(&mut chunk);
+    };
+    chunks.len().to_string()
 }
 
 #[actix_web::main]
