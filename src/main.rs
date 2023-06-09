05affd3e288bdb9113aeef8e58b0541a24098f53
Anush008 <anushshetty90@gmail.com>
2023-06-09 15:55:27 +0530

    chore: Bind all interfaces at 80

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() -> std::io::Result<()> {
             .service(routes::upload_jpg)
             .service(fs::Files::new("/images", "./images"))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 80))?
     .run()
     .await
 }
