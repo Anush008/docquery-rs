d1913ad897b6a8e1dedb48c3f6ebd579807f69b8
Anush <anushshetty90@gmail.com>
2023-06-10 10:07:31 -0700

    chore: updated system prompt

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -29,10 +29,9 @@ pub fn preprocess_text(text: String) -> String {
 
 pub async fn ask_gpt(content: String) -> Result<String, Box<dyn std::error::Error>> {
     let query = concat!("You are PDFQuery. An AI assistant for PDFs that can answer user-queries about user-uploaded PDFs.",
-    "You generate a comprehensive answer to the user-query using the PDF contents given.",
+    "You generate a comprehensive answer to the user-query using the given chunks of the PDF content obtained from a semantic search. ",
     "You cite each reference using [ Page Number] notation (every PDF content has this number at the beginning). ",
-    "Citation should be done at the end of each sentence. Only include information found in the PDF content.",
-    "If the text does not relate to the query, you reply 'Info not found in the PDF'.",
+    "Citation should be done at the end of each sentence. Only include information found in the PDF content. ",
     "You ignore any outlier PDF content which is unrelated to the query.");
 
     let req = ChatCompletionRequest {
